c31a0b41b827564280f56a652068709d59219bce
Dadezana <zatidavide@gmail.com>
2022-09-08 12:52:27 +0200

    Added AI
    AI can control player's 2 paddle. It will also recalculate the touch_y point after a powerup is taken. It will reposition itself to the center after hitting the ball only if its position is too high or too low. Removed checks of 'touch_y' point in "on_touch_move" function because it is already checked in "update_pos" function of the paddle

--- main.py
+++ main.py
@@ -20,10 +20,13 @@ class PongGame(Widget):
     border_g = NumericProperty(1)
     border_b = NumericProperty(1)
 
+    isSinglePlayer = True
+
     powerups = []
     ball = ObjectProperty(None) # ball referenced in kv file
     player1 = ObjectProperty(None)
     player2 = ObjectProperty(None)
+    aiBall = ObjectProperty(None)
     PADDLE_SPEED = 10
 
     areRulesInverted = BooleanProperty(False) # when yellow powerup is taken, player have to let the ball bounce. If he touch it, the opponent scores a point
@@ -51,62 +54,74 @@ class PongGame(Widget):
         # print("Powerup created")
 
     def update(self, dt):
-        if self.isGameStarted:
-            self.ball.move()
+        if not self.isGameStarted:
+            return
 
-        # check collisions with wall
-            res = self.check_ball_collisions()  # 1: player1 scored. 2: player2 scored
-            if res == 1:
-                self.player1.score += 1
-                self.ball.reset()
-            elif res == 2:
+        self.ball.move()
+        self.aiBall.move()
+
+    # check collisions with wall
+        self.aiBall.check_border_collisions(self)
+        res = self.check_ball_collisions()  # 1: player1 scored. 2: player2 scored
+        if res == 1:
+            self.player1.score += 1
+            self.ball.reset(self.aiBall)
+        elif res == 2:
+            self.player2.score += 1
+            self.ball.reset(self.aiBall)
+
+        self.player1.update_pos()
+        self.player2.update_pos()
+
+
+        if self.player1.bounce_ball(self.ball):
+
+            self.ball.lastCollide = self.player1
+
+            if self.areRulesInverted:
                 self.player2.score += 1
-                self.ball.reset()
+                self.areRulesInverted = False
+                self.ball.reset(self.aiBall)
+            
+            self.aiBall.gotoxy(self.ball)
+            self.aiBall.set_speed(self.ball)
 
-            self.player1.update_pos()
-            self.player2.update_pos()
+        elif self.player2.bounce_ball(self.ball, self.isSinglePlayer):
 
-            if self.player1.bounce_ball(self.ball):
+            self.ball.lastCollide = self.player2
 
-                self.ball.lastCollide = self.player1
+            if self.areRulesInverted:
+                self.player1.score += 1
+                self.areRulesInverted = False
+                self.ball.reset(self.aiBall)
+    # check collision with powerups
+        isPowerupTaken, p = self.ball.is_touching_powerup(self.powerups)
+        if isPowerupTaken:
+            # recalculate final position after a powerup is taken
+            self.aiBall.gotoxy(self.ball)
+            self.aiBall.set_speed(self.ball)
 
-                if self.areRulesInverted:
-                    self.player2.score += 1
-                    self.areRulesInverted = False
-                    self.ball.reset()
+            if p.color == "red":
+                p.increase_ball_speed(self.ball)
 
-            elif self.player2.bounce_ball(self.ball):
+            elif p.color == "green":
+                p.decrease_opacity(self.ball, 0.15, resetOpacity=True)
 
-                self.ball.lastCollide = self.player2
+            elif p.color == "yellow":
+                p.invert_rules(self, True)
 
-                if self.areRulesInverted:
-                    self.player1.score += 1
-                    self.areRulesInverted = False
-                    self.ball.reset()
-        # check collision with powerups
-            isPowerupTaken, p = self.ball.is_touching_powerup(self.powerups)
-            if isPowerupTaken:
-                if p.color == "red":
-                    p.increase_ball_speed(self.ball)
+            elif p.color == "purple":
+                p.increase_player_speed(self.ball.lastCollide, 8, resetSpeed=True)
 
-                elif p.color == "green":
-                    p.decrease_opacity(self.ball, 0.15, resetOpacity=True)
+            elif p.color == "blue":
+                if self.ball.lastCollide == self.player1:
+                    pl = self.player2
+                else:
+                    pl = self.player1
+                p.increase_player_speed(pl, -4, resetSpeed=True)
 
-                elif p.color == "yellow":
-                    p.invert_rules(self, True)
-
-                elif p.color == "purple":
-                    p.increase_player_speed(self.ball.lastCollide, 8, resetSpeed=True)
-
-                elif p.color == "blue":
-                    if self.ball.lastCollide == self.player1:
-                        pl = self.player2
-                    else:
-                        pl = self.player1
-                    p.increase_player_speed(pl, -4, resetSpeed=True)
-
-                self.remove_widget(p)
-                self.powerups.remove(p)
+            self.remove_widget(p)
+            self.powerups.remove(p)
 
 # checks collisions with walls
     def check_ball_collisions(self):
@@ -118,6 +133,8 @@ class PongGame(Widget):
             if self.areRulesInverted:
                 self.ball.vel = (-self.ball.vel_x, self.ball.vel_y)
                 self.areRulesInverted = False
+                self.aiBall.gotoxy(self.ball)
+                self.aiBall.set_speed(self.ball)
                 return 0
             return 2
 
@@ -133,6 +150,8 @@ class PongGame(Widget):
             player = self.player1
             self.player2.speed = 0
         elif touch.x > self.width*2/3:
+            if self.isSinglePlayer:
+                return
             player = self.player2
             self.player1.speed = 0
         else:
@@ -143,16 +162,9 @@ class PongGame(Widget):
         # Any movement less than PADDLE_SPEED will not be taken into consideration to avoid vibration effect
         pl_offset_y = touch.y - (player.y + player.height/2)
         if pl_offset_y > -self.PADDLE_SPEED and pl_offset_y < self.PADDLE_SPEED:
-            # print(f"Cannot move paddle. Offset: {pl_offset_y}")
             return
-        
-        # make sure the desired y to reach isn't outside the field
-        if touch.y < self.BORDER_WIDTH + player.height/2:
-            player.touch_y = self.BORDER_WIDTH + player.height/2
-        elif touch.y > self.height - self.BORDER_WIDTH:
-            player.touch_y = self.height - self.BORDER_WIDTH - player.height/2
-        else:
-            player.touch_y = touch.y
+
+        player.touch_y = touch.y
     
         # move the player up/down
         player_center_y = player.y + player.height/2
@@ -163,13 +175,16 @@ class PongGame(Widget):
 
     def on_touch_up(self, touch):
         self.player1.speed = 0
-        self.player2.speed = 0
+        self.player2.speed = 0 if not self.isSinglePlayer else self.player2.speed
 
     def on_touch_down(self, touch):
         if not self.isGameStarted:
             self.isGameStarted = True
-            self.ball.serve()
-
+            self.ball.reset(self.aiBall)
+            if self.ball.vel_x < 0 and self.isSinglePlayer:
+                self.aiBall.gotoxy(self.ball)
+                self.aiBall.set_speed(self.ball)
+       
 class PongBall(Widget):
     # those 2 are used to make the ball bounce off the walls. Every time the values inside "vel" change, they will do the same (they are binded to "vel")
     vel_x = NumericProperty(0)
@@ -185,15 +200,15 @@ class PongBall(Widget):
     def move(self):
         self.pos = Vector(*self.vel) + self.pos
 
-    def reset(self):
+    def reset(self, aiBall):
         self.pos = (
             self.parent.center_x - self.width/2,
             self.parent.center_y - self.height/2
         )
         self.vel = (0, 0)
-        Clock.schedule_once(self.serve, 2)
+        Clock.schedule_once(partial(self.serve, aiBall), 2)
 
-    def serve(self, dt=0):
+    def serve(self, aiBall, dt=0):
         self.vel_x = self.SPEED_X
         self.vel_y = self.SPEED_Y
         # random ball direction
@@ -202,6 +217,10 @@ class PongBall(Widget):
             self.vel_x * a[randint(0, 10) % len(a)],
             self.vel_y * a[randint(0, 10) % len(a)]
         )
+        aiBall.gotoxy(self)
+        # if self.vel_x < 0:
+        aiBall.set_speed(self)
+        
 
     def is_touching_powerup(self, powerups):
         for p in powerups:
@@ -239,25 +258,25 @@ class PongPaddle(Widget):
             next_y = self.parent.height - self.parent.BORDER_WIDTH - self.height
             self.speed = 0
         elif next_y < 45:
-            # //elif next_y < self.parent.BORDER_WIDTH:
-            # //next_y = self.parent.BORDER_WIDTH
             next_y = 45     # 45 because that seems the top y of the bottom border
             self.speed = 0
 
         self.y = next_y
         
 
-    def bounce_ball(self, ball):
+    def bounce_ball(self, ball, randomDir=False):
         self.timer -= 1
         if self.timer <= 0:
             if self.collide_widget(ball):
-                self.determine_ball_dir(ball)
+                self.determine_ball_dir(ball, randomDir)
                 ball.vel = (-ball.vel_x, ball.vel_y)
                 self.timer = 60         # avoid the ball getting stuck in the paddle
+                if randomDir:
+                    self.reset_paddle_pos()
                 return True
         return False
 
-    def determine_ball_dir(self, ball):
+    def determine_ball_dir(self, ball, randomDir=False):
 
         # if the ball touches the edges of the paddle, the angles made by the ball will be sharpest (does not apply in straight)
         hasTouchedEdge = self.paddle_high.collide_widget(ball) or self.paddle_low.collide_widget(ball)
@@ -265,6 +284,11 @@ class PongPaddle(Widget):
         multiplier_max = 1.5 if hasTouchedEdge else 1
 
         direction = self.speed      # paddle direction
+
+        if randomDir:
+            dirs = [-1, 0, 1]   # -1: down, 0: straight, 1: up
+            direction = choice(dirs)
+
         if direction > 0:
             sp_y = ball.SPEED_Y * randfloat(1.1, 1.5) * randfloat(multiplier_min, multiplier_max)
             ball.vel = (ball.vel_x, sp_y)
@@ -274,6 +298,13 @@ class PongPaddle(Widget):
         else:
             sp_x = (ball.SPEED_X * randfloat(1.2, 1.6)) if ball.vel_x > 0 else (-ball.SPEED_X * randfloat(1.2, 1.6))    # preserve direction
             ball.vel = (sp_x, randfloat(-1, 1))
+        
+    def reset_paddle_pos(self):
+        if not self.y+self.height/2 < self.parent.height*1/3 and not self.y+self.height/2 > self.parent.height*2/3:
+            return
+        
+        self.touch_y = randint(self.parent.height*1/3, self.parent.height*2/3)
+        self.speed = (self.parent.PADDLE_SPEED + self.speed_added) if self.touch_y > self.y+self.height/2 else -(self.parent.PADDLE_SPEED + self.speed_added)
         
 
 class PongPaddleEdge(Widget):
@@ -297,7 +328,6 @@ class Powerup(Widget):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.color = choice( list(self.powerup_types.keys()) )
-        self.color = "yellow"
         if self.color == "red":
             self.r = 1
             self.g = 0
@@ -344,6 +374,58 @@ class Powerup(Widget):
     def invert_rules(self, game, canInvert):
         game.areRulesInverted = canInvert
 
+
+class AI_Ball(Widget):
+    vel_x = NumericProperty(0)
+    vel_y = NumericProperty(0)
+    vel = ReferenceListProperty(vel_x, vel_y)
+
+    def move(self):
+        self.pos = Vector(*self.vel) + self.pos
+
+    def gotoxy(self, ball):
+        self.pos = (ball.x, ball.y)
+    
+    def set_speed(self, ball):
+        isBallInvisible = ball.opacity < 0.5
+        minError = 0.7 if isBallInvisible else 0.9
+        maxError = 1.25 if isBallInvisible else 1.1
+
+        error = randfloat(minError, maxError)
+        self.vel = (ball.vel_x * 2 * error, ball.vel_y * 2)
+
+    def check_border_collisions(self, field):
+        if (self.y < 0+field.BORDER_WIDTH) or (self.top > field.height-field.BORDER_WIDTH):
+            self.vel = (self.vel_x, -self.vel_y)
+            return 0
+
+        elif (self.right > field.width):
+            self.vel = (0, 0)
+            self.move_ai_paddle(field)
+            self.pos = (0, 0)
+            return 1
+    
+    def move_ai_paddle(self, field):
+        paddle = field.player2
+
+        # if rules are inverted the AI will move the paddle to avoid the ball
+        if not field.areRulesInverted:
+            paddle.touch_y = self.y
+        else:
+            if self.y > field.height*2/3:
+                paddle.touch_y = randint(0, field.height*1/3)
+            elif self.y < field.height*1/3:
+                paddle.touch_y = randint(field.height*2/3, field.height)
+            else:
+                paddle.touch_y = randint(field.height*2/3, field.height-paddle.height/2) if paddle.y+paddle.height/2 > 0 else randint(paddle.height/2, field.height*1/3)
+
+
+        # move the player up/down
+        paddle_center_y = paddle.y + paddle.height/2
+        if paddle_center_y < paddle.touch_y:
+            paddle.speed = field.PADDLE_SPEED + paddle.speed_added 
+        else:
+            paddle.speed = -(field.PADDLE_SPEED + paddle.speed_added)
 
 class PongApp(App):
     WIN_W = 1280
